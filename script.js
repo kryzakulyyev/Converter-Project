0cd867e69ffe483fa7f3f2d0e25c271ab482e4f8
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-10-06 18:14:05 -0700

    Add JavaScript Converter_explanations examples

---
+++ script.js
@@ -0,0 +1,30 @@
+//Kelvin to Celsius, then to Fahrenheit.
+
+//Kelvin value
+const kelvin=prompt("Enter the Kelvin value")*1;
+ 
+
+// Count Celsius value
+let celsius=kelvin-273 ;
+
+//Calculate Fahrenheit value;
+let fahrenheit=celsius*(9/5)+32;
+
+// Round the number saved to fahrenheit with Math method.
+fahrenheit=Math.round(fahrenheit); 
+
+ // Print the result  
+ console.log("The temperature is TEMPERATURE degrees Fahrenheit.");
+ 
+ alert(`${kelvin} kelvin equals to ${celsius} celsius which converts to ${fahrenheit} Fahrenheit`);
+  // Newton value floor, and print
+  const Newton=prompt("Enter the Speed ")*1;
+
+  let newton = celsius * (33/100);
+  let 
+
+
+console.log(newton); 
+
+console.log(Math.floor(newton));
+alert(`${newton} newton equals to ${celsiue} speed`);
